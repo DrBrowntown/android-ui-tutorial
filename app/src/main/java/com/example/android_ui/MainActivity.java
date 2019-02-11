bd087a93e58b68ca170ca34aebaf2d7186018ed6
drbrowntown <calvinbrownlee@gmail.com>
2019-02-11 15:31:19 -0500

    feat: Add methods to MainActivity

--- app/src/main/java/com/example/android_ui/MainActivity.java
+++ app/src/main/java/com/example/android_ui/MainActivity.java
@@ -10,4 +10,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calvin_b_linear_layout);
     }
+
+    @Override
+    protected  void onResume(){
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
 }
