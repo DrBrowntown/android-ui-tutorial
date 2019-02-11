a011b5b16d1d055fa8ced6f2de632af70bc14110
drbrowntown <calvinbrownlee@gmail.com>
2019-02-11 16:21:47 -0500

    feat: Log lifecycle in MainActivity

--- app/src/main/java/com/example/android_ui/MainActivity.java
+++ app/src/main/java/com/example/android_ui/MainActivity.java
@@ -2,42 +2,52 @@ package com.example.android_ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    final String TAG="MAIN ACTIVITY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calvin_b_linear_layout);
+
+        Log.i(TAG,"onCreate");
     }
 
     @Override
     protected  void onResume(){
         super.onResume();
+        Log.i(TAG,"onResume");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        Log.i(TAG, "onStart");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        Log.i(TAG, "onPause");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+        Log.i(TAG, "onStop");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        Log.i(TAG,"onDestroy");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
+        Log.i(TAG,"onRestart");
     }
 }
