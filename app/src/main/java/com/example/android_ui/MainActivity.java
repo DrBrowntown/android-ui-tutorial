512ced136d7305bf7902c244c9472cb391c0ae5d
drbrowntown <calvinbrownlee@gmail.com>
2019-02-12 10:33:29 -0500

    feat: Hook up event listeners to input fields and button for login

--- app/src/main/java/com/example/android_ui/MainActivity.java
+++ app/src/main/java/com/example/android_ui/MainActivity.java
@@ -1,22 +1,59 @@
 package com.example.android_ui;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     final String TAG="MAIN ACTIVITY";
+    final String USERNAME="CALVIN";
+    final String PASSWORD="PASSWORD";
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
 
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calvin_b_linear_layout);
 
+        usernameEditText = (EditText) findViewById(R.id.username_edit_text);
+        passwordEditText = (EditText) findViewById(R.id.password_edit_text);
+
+
+        loginButton = (Button)findViewById(R.id.login_button);
+        loginButton.setOnClickListener(this);
+//        button.setOnClickListener(new OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+//                startActivity(intent);
+//            }
+//        });
         Log.i(TAG,"onCreate");
 
-        Intent intent=new Intent(this, SecondActivity.class);
-        startActivity(intent);
+
+        try {
+            InputStream inputStream= getAssets().open("ic_launcher.png");
+            Drawable drawable=Drawable.createFromStream(inputStream, null);
+            loginButton.setBackground(drawable);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
     @Override
@@ -24,6 +61,8 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         Log.i(TAG,"onResume");
     }
+
+
 
     @Override
     protected void onStart() {
@@ -53,5 +92,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onRestart() {
         super.onRestart();
         Log.i(TAG,"onRestart");
+    }
+
+    @Override
+    public void onClick(View v) {
+        int viewId = view.getId();
+
+        switch (viewId) {
+            case R.id.login_button:
+                String username = usernameEditText.getEditableText().toString();
+                String password = passwordEditText.getEditableText().toString();
+                break;
+        }
     }
 }
